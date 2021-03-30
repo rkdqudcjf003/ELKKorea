14df5f89b0000c4453ca71e9c8c4c924ecc158fe
rkdqudcjf003 <62277356+rkdqudcjf003@users.noreply.github.com>
2021-03-30 15:26:44 +0900

    Rkdqudcjf (#2)
    * java 파일생성
    
    * dbconfig

--- ELKKorea/src/main/java/com/project/elk/config/DBConfiguration.java
+++ ELKKorea/src/main/java/com/project/elk/config/DBConfiguration.java
@@ -1,5 +1,67 @@
 package com.project.elk.config;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.project.elk.ElkKoreaApplication;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@PropertySource("classpath:/application.yml")
 public class DBConfiguration {
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Bean
+	@ConfigurationProperties(prefix = "mybatis.configuration")
+	public org.apache.ibatis.session.Configuration mybatisConfg() {
+		return new org.apache.ibatis.session.Configuration();
+	}
+
+	private static final Logger log = LoggerFactory.getLogger(ElkKoreaApplication.class);
+
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource.hikari")
+	public HikariConfig hikariConfig() {
+		return new HikariConfig();
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DataSource dataSource = new HikariDataSource(hikariConfig());
+		log.info("datasource : {}", dataSource);
+		return dataSource;
+	}
+
+	/*
+	 * SqlSessionFactory 설정
+	 */
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		sessionFactory.setTypeAliasesPackage("com.project.elk.*");
+		sessionFactory.setMapperLocations(applicationContext.getResources("classpath:com.project.elk.*.mapper/*.xml"));
+		sessionFactory.setConfiguration(mybatisConfg());
+		return sessionFactory.getObject();
+
+	}
+
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
+	}
 
 }
