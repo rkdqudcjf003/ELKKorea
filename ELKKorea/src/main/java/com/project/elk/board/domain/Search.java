6a53f1bd93585eee1aa2aaac9a74cc3f9c1fef46
rkdqudcjf003 <rkdqudcjf003@naver.com>
2021-04-11 13:26:13 +0900

    page search

---
+++ ELKKorea/src/main/java/com/project/elk/board/domain/Search.java
@@ -0,0 +1,25 @@
+package com.project.elk.board.domain;
+
+import lombok.Data;
+
+@Data
+public class Search extends PageDTO {
+	private String searchType;
+	private String keyword;
+
+	public String getSearchType() {
+		return searchType;
+	}
+
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+}
