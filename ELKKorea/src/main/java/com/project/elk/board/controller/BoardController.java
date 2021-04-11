08626b6fa016c84d3c9cf087106954c8edbc8484
rkdqudcjf003 <rkdqudcjf003@naver.com>
2021-04-11 13:18:27 +0900

    board11111

--- ELKKorea/src/main/java/com/project/elk/board/controller/BoardController.java
+++ ELKKorea/src/main/java/com/project/elk/board/controller/BoardController.java
@@ -12,13 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.elk.board.domain.BoardVO;
 import com.project.elk.board.service.BoardService;
-import com.project.elk.board.domain.PageDTO;
-import com.project.elk.board.domain.Search;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
